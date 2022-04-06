ad3548f869792f11e47d00293905ad6ec5f2fe1a
roykho <roykho77@gmail.com>
2022-04-06 05:50:58 -0700

    Add hook

--- plugins/woocommerce/includes/class-woocommerce.php
+++ plugins/woocommerce/includes/class-woocommerce.php
@@ -649,7 +649,7 @@ final class WooCommerce {
 	public function load_plugin_textdomain() {
 		$locale = determine_locale();
 		$locale = apply_filters( 'plugin_locale', $locale, 'woocommerce' );
-
+do_action('woocommerce_test_hook');
 		unload_textdomain( 'woocommerce' );
 		load_textdomain( 'woocommerce', WP_LANG_DIR . '/woocommerce/woocommerce-' . $locale . '.mo' );
 		load_plugin_textdomain( 'woocommerce', false, plugin_basename( dirname( WC_PLUGIN_FILE ) ) . '/i18n/languages' );
